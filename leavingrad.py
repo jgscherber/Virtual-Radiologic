e3d64a3436e809760c7260b881b7e977540866b0
jgscherber <jgscherber@gmail.com>
2016-07-13 15:51:36 -0500

    submission added
    added script from privileges.py to submit information gathered

--- leavingrad.py
+++ leavingrad.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-from datetime import datetime
+import datetime
 from dateutil.relativedelta import relativedelta
 import time, sys, os, calendar, openpyxl as oxl
 
@@ -54,21 +54,24 @@ for i in range(len(ID_nums)):
         actions.move_to_element(rads_in)
         actions.click()
         actions.send_keys(ID_nums[i]).perform()
-        time.sleep(0.5)
+        time.sleep(1)
         actions = ActionChains(driver)
         actions.send_keys(Keys.RETURN).perform()
     except:
-        for i in range(2,len(ID_nums[i])):
-            actions = ActionChains(driver)
-            actions.send_keys(Keys.BACK_SPACE).perform()
+        print('ID: {0} is no longer in the system'
+              ', remove and restart'.format(ID_nums[i-1]))
+        driver.quit()
+        sys.exit()
     continue
 # run and export
 driver.find_element_by_class_name('pull-right').click()
 time.sleep(7)
 driver.find_element_by_class_name('headerButton').click()
-time.sleep(1)
+time.sleep(7)
+driver.quit()
 ## Comparing Data
 test_wb = oxl.load_workbook('output.xlsx')
+os.remove('output.xlsx')
 test = test_wb.get_sheet_by_name('Worklist')
 t_length = 1
 
@@ -84,10 +87,10 @@ positives = []
 for row in range(2, t_length):
     current_record = []
     if test['S{0}'.format(row)].value == "":
-        test['S{0}'.format(row)].value = datetime.strptime(
+        test['S{0}'.format(row)].value = datetime.datetime.strptime(
             '01/01/2099', '%m/%d/%Y')
     else:
-        test['S{0}'.format(row)].value = datetime.strptime(
+        test['S{0}'.format(row)].value = datetime.datetime.strptime(
                                     test['S{0}'.format(row)].value, '%m/%d/%Y')
     for rad in range(len(ID_nums)):
         if (ID_nums[rad] == str(test['A{0}'.format(row)].value) and
@@ -102,7 +105,41 @@ for row in range(2, t_length):
             positives.append([date_reform,
                               test['B{0}'.format(row)].value,
                               test['E{0}'.format(row)].value])
-print(positives)
+driver = webdriver.Chrome()
+driver.get('http://vradhome/Privileging/Facility?facility')
+for k in range(len(positives)):
+    fac_search = driver.find_element_by_id('s2id_autogen1')
+    actions = ActionChains(driver)
+    actions.move_to_element(fac_search)
+    actions.click()
+    actions.send_keys(positives[k][2]).perform()
+    time.sleep(1.5)
+    actions = ActionChains(driver)
+    actions.send_keys(Keys.RETURN).perform()
+    time.sleep(5)
+
+    driver.find_element_by_xpath("//*[contains(text(), '{0}')]".format(
+        positives[k][1])).click()
+    expand = driver.find_elements_by_class_name("expand")[2].click()
+    tomorrow = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%m/%d/%y')
+
+    next_act = driver.find_elements_by_class_name("form-control")[0]
+    comment = driver.find_elements_by_class_name("form-control")[9]
+    actions = ActionChains(driver)
+    actions.move_to_element(next_act).click()
+    actions.send_keys('{0}'.format(tomorrow)).send_keys(Keys.TAB)
+    actions.send_keys("See Note (Rad Leaving)").perform()
+    actions.move_to_element(comment).click().send_keys(
+        'Rad last read date on: {0}. Please stop app'
+        ' process and update status to WD. Please contact'
+        ' Roberta with questions.'.format(positives[k][0])
+        ).perform()
+    driver.find_element_by_xpath('//*[@title="Submit Mass Update"]').click()
+    print('See Note (Rad Leaving)\n{0} : {1} \nRad last read date on: {2}.'
+          ' Please stop app process and update status to WD.'
+          ' Please contact Roberta with questions.\n'.format(
+              positives[k][1],positives[k][2],positives[k][0]))
+    time.sleep(3)
 # ID = A
 # Name = B
 # Facility = E
@@ -112,4 +149,4 @@ print(positives)
 
 
 
-os.remove('output.xlsx')
+
