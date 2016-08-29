cddba16f69546b0ca8b33627c2f15aec41603e6a
jgscherber <jgscherber@gmail.com>
2016-08-29 15:20:26 -0500

    Add files via upload
    added function to remove records that don't exist anymore. May need more work, on initial test, deleted multiple items (including correct one and incorrect last item)

--- leavingrad.py
+++ leavingrad.py
@@ -18,11 +18,43 @@ def strip_rad_reference():
         if sheet['B{0}'.format(j)].value == None:
             break
         else:
-            ID_nums.append(str(sheet['B{0}'.format(j)].value))
             leave_date.append(sheet['A{0}'.format(j)].value)
+            ID_nums.append(str(sheet['B{0}'.format(j)].value))
         j += 1
     return ID_nums, leave_date
 
+def remove_and_restart(bad_ID, driver):
+    driver.quit()
+    refer = oxl.load_workbook('rad_reference.xlsx')
+    sheet = refer.get_sheet_by_name('Sheet1')
+    ID_nums = []
+    leave_date = []
+    names = []
+    j = 2
+    while True:
+        if sheet['B{0}'.format(j)].value == None:
+            break
+        else:
+            leave_date.append(sheet['A{0}'.format(j)].value)
+            sheet['A{0}'.format(j)].value = None
+            ID_nums.append(str(sheet['B{0}'.format(j)].value))
+            sheet['B{0}'.format(j)].value = None        
+            names.append(sheet['C{0}'.format(j)].value)
+            sheet['C{0}'.format(j)].value = None
+        j += 1
+    for i in reversed(range(len(ID_nums))):
+        if ID_nums[i] == bad_ID:
+            del(ID_nums[i])
+            del(leave_date[i])
+            del(names[i])
+    for i in range(2,len(ID_nums)+2):
+        sheet['A{0}'.format(i)].value = leave_date[i-2]
+        sheet['B{0}'.format(i)].value = int(ID_nums[i-2])
+        sheet['C{0}'.format(i)].value = names[i-2]
+    refer.save('rad_reference.xlsx')
+    # need to restart script
+    execfile('leavingrad.py')
+    
 def get_output(ID_nums):
     # navigate to the rad priv report
     fp = webdriver.FirefoxProfile()
@@ -60,9 +92,11 @@ def get_output(ID_nums):
             actions = ActionChains(driver)
             actions.send_keys(Keys.RETURN).perform()
         except:
+            # need to add code to remove the record automatically and then restart
             print('ID: {0} is no longer in the system'
                   ', remove and restart'.format(ID_nums[i-1]))
-            driver.quit()
+            remove_and_restart(ID_nums[i-1], driver)
+            time.sleep(2)
             sys.exit()
         continue
     # run and export
@@ -111,9 +145,9 @@ def get_positives(ID_nums, leave_date, remove=True):
                 date_reform = leave_date[rad].strftime('%m/%d/%Y')
                 positives.append([date_reform,
                                   test['B{0}'.format(row)].value,
-                                  (test['E{0}'.format(row)].value] + ' ' + test['F{0}'.format(row)].value))
+                                  (test['E{0}'.format(row)].value + ' ' + test['F{0}'.format(row)].value)])
     return positives
-
+# how to deal with C2 sites
 
 def input_information(positives):
     driver = webdriver.Chrome()
@@ -125,44 +159,43 @@ def input_information(positives):
         actions = ActionChains(driver)
         actions.move_to_element(fac_search)
         actions.click()
-
-        # need to add a space + state initials after site names to ensure correct
-        # site is selected (e.g. St Francis Medical Center)
-
         actions.send_keys(positives[k][2]).perform()
         time.sleep(1.5)
         actions = ActionChains(driver)
         actions.send_keys(Keys.RETURN).perform()
         time.sleep(10)
-        try:
-            name_spot = driver.find_element_by_xpath(
-                "//*[contains(text(), '{0}')]".format(positives[k][1]))
-            actions = ActionChains(driver)
-            actions.move_to_element(name_spot)
-            actions.click()
-            actions.perform()
-            expand = driver.find_elements_by_class_name("expand")[2].click()
-            tomorrow = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%m/%d/%y')
-            next_act = driver.find_elements_by_class_name("form-control")[0]
-            comment = driver.find_elements_by_class_name("form-control")[9]
-            actions = ActionChains(driver)
-            actions.move_to_element(next_act).click()
-            actions.send_keys('{0}'.format(tomorrow)).send_keys(Keys.TAB)
-            actions.send_keys("See Note (Rad Leaving)").perform()
-            actions.move_to_element(comment).click().send_keys(
-                'Rad last read date on: {0}. Please stop app'
-                ' process and update status to WD. Please contact'
-                ' Roberta with questions.'.format(positives[k][0])
-                ).perform()
-            driver.find_element_by_xpath('//*[@title="Submit Mass Update"]').click()
-            print('See Note (Rad Leaving)\n{0} : {1} \nRad last read date on: {2}.'
-              ' Please stop app process and update status to WD.'
-              ' Please contact Roberta with questions.\n'.format(
-                  positives[k][1],positives[k][2],positives[k][0]))
-            
-        except selenium.common.exceptions.NoSuchElementException:
-                print("unable to find {0}".format(positives[k][1]))
-        continue
+        ex_test = driver.find_elements_by_class_name("expand")
+        # maybe avoide C2
+        if len(ex_test) < 3:
+            continue
+        else:
+            try:
+                name_spot = driver.find_element_by_xpath(
+                    "//*[contains(text(), '{0}')]".format(positives[k][1]))
+                actions = ActionChains(driver)
+                actions.move_to_element(name_spot)
+                actions.click()
+                actions.perform()
+                expand = driver.find_elements_by_class_name("expand")[2].click()
+                tomorrow = (datetime.date.today() + datetime.timedelta(days=1)).strftime('%m/%d/%y')
+                next_act = driver.find_elements_by_class_name("form-control")[0]
+                comment = driver.find_elements_by_class_name("form-control")[9]
+                actions = ActionChains(driver)
+                actions.move_to_element(next_act).click()
+                actions.send_keys('{0}'.format(tomorrow)).send_keys(Keys.TAB)
+                actions.send_keys("See Note (Rad Leaving)").perform()
+                actions.move_to_element(comment).click().send_keys(
+                    'Rad last read date on: {0}. Please stop app'
+                    ' process and update status to WD. Please contact'
+                    ' Roberta with questions.'.format(positives[k][0])
+                    ).perform()
+                driver.find_element_by_xpath('//*[@title="Submit Mass Update"]').click()
+                print('{0} : {1} \nRad last read date on: {2}.'.format(
+                      positives[k][1],positives[k][2],positives[k][0]))
+                
+            except selenium.common.exceptions.NoSuchElementException:
+                    print("** Unable to find {0} at {1} **\n".format(positives[k][1],positives[k][2]))
+            continue
           
         
         time.sleep(3)
@@ -170,6 +203,6 @@ def input_information(positives):
 
 ID_nums, leave_date = strip_rad_reference()    
 get_output(ID_nums)
-positives = get_positives(ID_nums, leave_date, remove=False)
+positives = get_positives(ID_nums, leave_date)
 input_information(positives)
 
